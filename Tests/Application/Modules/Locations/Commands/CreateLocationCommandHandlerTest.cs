ec361594137074363672bafaee76615ff768b3be
Oleksii Konstantyniuk <oleksii_konstantyniuk@epam.com>
2022-02-01 11:55:31 +0200

    Added UT

---
+++ Tests/Application/Modules/Locations/Commands/CreateLocationCommandHandlerTest.cs
@@ -0,0 +1,48 @@
+﻿using Application.Dtos.Locations;
+using Application.Modules.Locations.Commands.CreateLocation;
+
+namespace Tests.Application.Modules.Locations.Commands;
+
+[TestClass]
+public class CreateLocationCommandHandlerTest
+{
+    private readonly IGenericRepository<Location> _locationsRepository = Substitute.For<IGenericRepository<Location>>();
+    private readonly ILocationsService _locationsService = Substitute.For<ILocationsService>();
+    private readonly IMapper _mapper = Substitute.For<IMapper>();
+
+    private readonly CancellationToken _cancellationToken = new();
+    private readonly CreateLocationCommandRequest _commandRequest = new() { Details = new LocationDetailsDto() };
+
+    private readonly CreateLocationCommandHandler _commandHandler;
+
+    public CreateLocationCommandHandlerTest() 
+        => _commandHandler = new CreateLocationCommandHandler(_locationsRepository, _locationsService, _mapper);
+
+    [TestMethod]
+    [ExpectedException(typeof(ExistsException))]
+    public async Task Handle_LocationExists_ExpectedException()
+    {
+        // Arrange
+        var sameLocations = new Location[] { new Location() };
+        _locationsService.GetSameLocations(_commandRequest.Details).Returns(sameLocations);
+
+        // Act
+        _ = await _commandHandler.Handle(_commandRequest, _cancellationToken);
+    }
+
+    [TestMethod]
+    public async Task Handle_HappyPath_ReturnsCreatedLocationId()
+    {
+        // Arrange
+        var sameLocations = Array.Empty<Location>();
+        _locationsService.GetSameLocations(_commandRequest.Details).Returns(sameLocations);
+
+        _mapper.Map<Location>(_commandRequest.Details).Returns(new Location());
+
+        // Act
+        var commandResult = await _commandHandler.Handle(_commandRequest, _cancellationToken);
+
+        // Assert
+        commandResult.Data.Should().NotBeEmpty();
+    }
+}
---
+++ Tests/Application/Modules/Locations/Commands/DeleteLocationCommandHandlerTest.cs
@@ -0,0 +1,42 @@
+﻿using Application.Modules.Locations.Commands.DeleteLocation;
+
+namespace Tests.Application.Modules.Locations.Commands;
+
+[TestClass]
+public class DeleteLocationCommandHandlerTest
+{
+    private readonly IGenericRepository<Location> _locationsRepository = Substitute.For<IGenericRepository<Location>>();
+
+    private readonly CancellationToken _cancellationToken = new();
+    private readonly DeleteLocationCommandRequest _commandRequest = new() { Id = new Guid() };
+
+    private readonly DeleteLocationCommandHandler _commandHandler;
+
+    public DeleteLocationCommandHandlerTest()
+        => _commandHandler = new DeleteLocationCommandHandler(_locationsRepository);
+
+    [TestMethod]
+    [ExpectedException(typeof(NotFoundException))]
+    public async Task Handle_LocationNotFound_ExpectedException()
+    {
+        // Arrange
+        _locationsRepository.GetByIdAsync(_commandRequest.Id).Returns((Location)null);
+
+        // Act
+        _ = await _commandHandler.Handle(_commandRequest, _cancellationToken);
+    }
+
+    [TestMethod]
+    public async Task Handle_HappyPath_ExpectedDeletedLocation()
+    {
+        // Arrange
+        var location = new Location { Id = _commandRequest.Id };
+        _locationsRepository.GetByIdAsync(_commandRequest.Id).Returns(location);
+
+        // Act
+        _ = await _commandHandler.Handle(_commandRequest, _cancellationToken);
+
+        // Assert
+        await _locationsRepository.Received(1).DeleteAsync(Arg.Is<Location>(x => x.Id == _commandRequest.Id));
+    }
+}
